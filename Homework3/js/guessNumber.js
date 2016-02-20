be2d44abb45b95bf9188d174bfb990df9e1d701c
CharlotteAtWork <CharlotteAtWork@gmail.com>
2016-02-20 14:38:15 +0800

    guess number done

--- Homework3/js/guessNumber.js
+++ Homework3/js/guessNumber.js
@@ -1,23 +1,34 @@
 // Computer random number
 var computerNum = [];
-computerNum[0] = Math.floor((Math.random() * 10) % 9) + 1;
 var cptIndex = 1;
+var userInputNum = [];
+var inputIndex = 0;
+var guessCount = 0;
 
-do {
-    var tempValue = Math.floor((Math.random() * 10) % 9) + 1;
-    var isSameNum = false;
-    for (var i = 0; i < computerNum.length; i++) {
-        if (tempValue == computerNum[i]) {
-            isSameNum = true;
-            break;
+function computerNumber() {
+    // reset data
+    computerNum = [];
+    computerNum[0] = Math.floor((Math.random() * 10) % 9) + 1;
+    cptIndex = 1;
+    guessCount = 0;
+    document.getElementById("congratsPanel").style.display = "none";
+
+    do {
+        var tempValue = Math.floor((Math.random() * 10) % 9) + 1;
+        var isSameNum = false;
+        for (var i = 0; i < computerNum.length; i++) {
+            if (tempValue == computerNum[i]) {
+                isSameNum = true;
+                break;
+            }
+        }; // for
+
+        if (!isSameNum) {
+            computerNum[cptIndex] = tempValue;
+            cptIndex++;
         }
-    }; // for
-
-    if (!isSameNum) {
-        computerNum[cptIndex] = tempValue;
-        cptIndex++;
-    }
-} while (cptIndex < 4);
+    } while (cptIndex < 4);
+}
 
 function showRulePan() {
     document.getElementById("rulePanel").style.display = "block";
@@ -27,11 +38,9 @@ function hideRulePan() {
     document.getElementById("rulePanel").style.display = "none";
 }
 
-function init() {
+function guessNumber() {
     // User input number
     var numberInputPan = document.getElementsByClassName("numberInputPan")[0];
-    var userInputNum = [];
-    var inputIndex = 0;
     numberInputPan.onkeyup = function(e) {
         var target = e.srcElement;
         if (/\D/g.test(target.value)) {
@@ -69,11 +78,78 @@ function init() {
                     } // while
                 } // if myLength >= maxLength
 
-                // if user input 4 non-repeated digit, then auto compare
-                console.log("Computer random number: " + computerNum.toString());
-                console.log("User inputer number: " + userInputNum.toString());
+                if (inputIndex == 4) {
+                    // if user input 4 non-repeated digit, then auto compare
+                    displayGuessingResult();
+                } // if already input 4 digit
             } // if hasSameNumber
         } // if not a number
     };
+}
+
+function displayGuessingResult() {
+    console.log("Computer random number: " + computerNum.toString());
+    console.log("User input number: " + userInputNum.toString());
+    guessCount++;
+    var countA = 0;
+    var countB = 0;
+    for (var k = 0; k < 4; k++) {
+        if (userInputNum[k] == computerNum[k]) {
+            countA++;
+        }
+    } // for
+    console.log("both the number AND the postion are CORRECT: " + countA);
+
+    for (var l = 0; l < 4; l++) {
+        for (var m = 0; m < 4; m++) {
+            if (l != m && userInputNum[l] == computerNum[m]) {
+                countB++;
+                break;
+            }
+        }
+    } // for
+    console.log("the correct number BUT in the WRONG spot: " + countB);
+
+    // process page
+    var guessTable = document.getElementById("guessTable");
+    var guessTableLastIndex = guessTable.rows.length;
+    //console.log("guessTable Last Index: " + guessTableLastIndex);
+    var row = guessTable.insertRow(guessTableLastIndex);
+    var cell1 = row.insertCell(0);
+    var cell2 = row.insertCell(1);
+    var cell3 = row.insertCell(2);
+    cell1.innerHTML = userInputNum.toString();
+    cell2.innerHTML = " <----- 第 " + guessCount + " 次猜測結果 ----->";
+    cell3.innerHTML = countA + "A" + countB + "B";
+
+    if (countA == 4) {
+        document.getElementById("answerDiv").innerHTML = "電腦數字是：" + computerNum.toString();
+        document.getElementById("congratsPanel").style.display = "block";
+    } else
+        cleanUserInput();
+}
+
+function cleanUserInput() {
+    for (var k = 0; k < 4; k++) {
+        document.getElementById("num" + k).value = "";
+    } // for
+    userInputNum = [];
+    inputIndex = 0;
+    document.getElementById("num0").focus();
+}
+
+function playAgain() {
+    cleanUserInput();
+    var guessTable = document.getElementById("guessTable");
+    for (var i = 0; i < guessTable.rows.length; i++) {
+        guessTable.deleteRow(0);
+    }
+
+    computerNumber();
+}
+
+function init() {
+    computerNumber();
+    guessNumber();
 }
 window.onload = init;
